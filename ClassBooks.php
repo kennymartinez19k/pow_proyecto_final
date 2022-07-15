f496ea63cae70d31df1de467d1b5212fdf956809
kennymartinez19k <kennymartinez19k@gmail.com>
2022-07-15 14:42:45 +0000

    add changes

--- ClassBooks.php
+++ ClassBooks.php
@@ -1,25 +1,17 @@
 <?php
-
-    include('DataBase.php');
+// 
+    // include('DataBase.php');
 
     class Title {
-        private $id;
-        private $name;
-        private $last_name;
-        private $phone;
-        private $address;
-        private $city;
-        private $state;
-        private $country;
-        private $postal_code;
+        
 
         public function showTitle (){
-           $baseData = new DataBase();
-            $info = $baseData-> connectDataBase();
-            $titles = $info->query("SELECT * FROM titulos");
-            $titles-> execute();
-            $listTitles = $titles->fetchAll(PDO::FETCH_ASSOC);
-            return $listTitles;
+        //    $baseData = new DataBase();
+        //     $info = $baseData-> connectDataBase();
+        //     $titles = $info->query("SELECT * FROM titulos");
+        //     $titles-> execute();
+        //     $listTitles = $titles->fetchAll(PDO::FETCH_ASSOC);
+            return [];
 
          }
     }
